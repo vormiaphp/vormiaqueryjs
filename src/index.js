0757edc46f82f2dbaf2047b548cd48c25eeed443
Josh L. Minga <joshlminga@gmail.com>
2025-07-03 18:32:55 +0300

    The build error is caused by trying to export useVrmAuth from src/hooks/useVrmAuth.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // Main entry point
-export { useVrmQuery } from './hooks/useVrmQuery.js';
-export { useVrmAuth } from './hooks/useVrmAuth.js';
-export { useVrmMutation } from './hooks/useVrmMutation.js';
-export { createVormiaClient } from './client/createVormiaClient.js';
+export { useVrmQuery } from "./hooks/useVrmQuery.js";
+export {
+  useVormiaQueryAuth,
+  useVormiaQueryAuthMutation,
+} from "./hooks/useVrmAuth.js";
+export { useVrmMutation } from "./hooks/useVrmMutation.js";
+export { createVormiaClient } from "./client/createVormiaClient.js";
 
 // Export types as plain objects for documentation purposes
 export const VormiaError = {};
@@ -13,11 +16,11 @@ export const VormiaMutationOptions = {};
 export const VormiaResponse = {};
 export const VormiaAuthResponse = {};
 export const HttpMethod = {
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
-  PATCH: 'PATCH',
-  DELETE: 'DELETE',
-  HEAD: 'HEAD',
-  OPTIONS: 'OPTIONS'
+  GET: "GET",
+  POST: "POST",
+  PUT: "PUT",
+  PATCH: "PATCH",
+  DELETE: "DELETE",
+  HEAD: "HEAD",
+  OPTIONS: "OPTIONS",
 };
